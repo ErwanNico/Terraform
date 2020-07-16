2393eeb6b87c917d38fc3dd125a778487004e36d
Erwan NICO <erwan.nico@epsi.fr>
2020-07-16 07:26:12 +0000

    public_c

--- main.tf
+++ main.tf
@@ -1,73 +1,116 @@
-# Configure the AWS Provider
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
 }
-######################################## RESEAU ########################################
-# Create a VPC
-resource "aws_vpc" "vpc_terraform" {
-  cidr_block = "10.10.0.0/16"
+
+resource "aws_subnet" "public-a" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.1.0/24"
+
   tags = {
-    Name = "vpc_terraform"
-    Env = "tp"
+    Name = "public-a-tf"
   }
 }
 
-# Create subnet
-resource "aws_subnet" "subnet_1_terraform" {
-  cidr_block = "10.10.1.0/24"
-  vpc_id = aws_vpc.vpc_terraform.id
+resource "aws_subnet" "public-b" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "public-b-tf"
+  }
+}
+
+resource "aws_subnet" "public-c" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.7.0/24"
+
+  tags = {
+    Name = "public-c-tf"
+  }
+}
+
+resource "aws_subnet" "private-a" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "private-a-tf"
+  }
+}
+
+resource "aws_subnet" "private-b" {
+  vpc_id     = aws_vpc.default.id
+  cidr_block = "10.0.4.0/24"
+
+  tags = {
+    Name = "private-b-tf"
+  }
+}
+
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
   
   tags = {
-    Name = "subnet_1_terraform"
-    Env = "tp"
-   }
-  }
-
-  # Create gateway
-  resource "aws_internet_gateway" "internetgateway_terraform" {
-  vpc_id = aws_vpc.vpc_terraform.id
-
-  tags = {
-    Name = "internetgateway_terraform"
-    Env = "tp"
+    Name = "terraform"
   }
 }
 
-# Create routetable
-resource "aws_route_table" "routetable_terraform" {
-  vpc_id = aws_vpc.vpc_terraform.id
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.default.id
+
+  tags = {
+    Name = "igw-tf"
+  }
+}
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.default.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internetgateway_terraform.id
+    gateway_id = aws_internet_gateway.gw.id
   }
+  tags = {
+    Name = "internet-tf"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public-a.id
+  route_table_id = aws_route_table.r.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public-b.id
+  route_table_id = aws_route_table.r.id
+}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "ec2-key-tf"
+  public_key = tls_private_key.example.public_key_openssh
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public-b.id
+  key_name      = aws_key_pair.deployer.id
+  associate_public_ip_address = true
+  user_data     = file("${path.module}/postinstall.sh")
+  vpc_security_group_ids =  ["${aws_security_group.allow_http.id}"]
 
   tags = {
-    Name = "routetable_terraform"
-    Env = "tp"
+    Name = "HelloWorld"
   }
 }
 
-#Create association route table
-resource "aws_route_table_association" "routetableassociation" {
-  subnet_id      = aws_subnet.subnet_1_terraform.id
-  route_table_id = aws_route_table.routetable_terraform.id
-}
-######################################## END RESEAU ########################################
-
-#Create clé RSA 4096
-resource "tls_private_key" "key_terraform" {
-  algorithm   = "RSA"
-  rsa_bits = "4096"
-}
-resource "aws_key_pair" "ec2-key-tf" {
-  key_name   = "ec2-key-tf"
-  public_key = tls_private_key.key_terraform.public_key_openssh
-}
-
-######################################## INSTANCE ########################################
-#Create EC2
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -81,91 +124,41 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 
-  owners = ["099720109477"] # Canonical
+  owners = ["099720109477"] # Canonical 
 }
 
-resource "aws_instance" "ec2-test-1" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.subnet_1_terraform.id
-  key_name   = aws_key_pair.ec2-key-tf.id
-  associate_public_ip_address = true
-  user_data = file("${path.module}/post_install.sh")
-  vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_ssh_vpc.id]
-
-  tags = {
-    Name = "ec2-test-1"
-  }
-}
-resource "aws_instance" "ec2-test-2" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id = aws_subnet.subnet_1_terraform.id
-  key_name   = aws_key_pair.ec2-key-tf.id
-  associate_public_ip_address = true
-  user_data = file("${path.module}/post_install.sh")
-  vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_ssh_vpc.id]
-
-  tags = {
-    Name = "ec2-test-2"
-  }
-}
-
-
-######################################## END INSTANCE ########################################
-
-######################################## SECURITY GROUP ########################################
 resource "aws_security_group" "allow_http" {
-  name        = "allow_http_from_any"
+  name        = "allow_tls"
   description = "Allow http inbound traffic"
-  vpc_id      = aws_vpc.vpc_terraform.id
+  vpc_id      = "${aws_vpc.default.id}"
 
   ingress {
-    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "allow_http"
+    Name = "allow_http-tf"
   }
 }
-resource "aws_security_group" "allow_ssh_vpc" {
-  name        = "allow_ssh_from_vpc"
-  description = "Allow ssh inbound traffic"
-  vpc_id      = aws_vpc.vpc_terraform.id
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.vpc_terraform.cidr_block]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh_vpc"
-  }
+output "private-key" {
+  value = tls_private_key.example.private_key_pem
 }
-######################################## END SECURITY GROUP ########################################
 
-#OUTPUT
-output "private_key" {
-  value = tls_private_key.key_terraform.private_key_pem
+output "ami-value" {
+  value = data.aws_ami.ubuntu.image_id
+}
+
+output "public-ip" {
+  value = aws_instance.web.public_ip
 }
