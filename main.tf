dbb8a67be4c64b4b3bf3748aef6c942b9a05edc1
ErwanNico <43747860+ErwanNico@users.noreply.github.com>
2020-07-16 16:43:38 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
+###NICO Erwan
+
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-1"
 }
 
+####VPC
+resource "aws_vpc" "vpc_terraform_tp" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "terraform"
+  }
+}
+
+####SUBNET
 resource "aws_subnet" "public-a" {
-  vpc_id     = aws_vpc.default.id
+  vpc_id     = aws_vpc.vpc_terraform_tp.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
@@ -13,7 +25,7 @@ resource "aws_subnet" "public-a" {
 }
 
 resource "aws_subnet" "public-b" {
-  vpc_id     = aws_vpc.default.id
+  vpc_id     = aws_vpc.vpc_terraform_tp.id
   cidr_block = "10.0.2.0/24"
 
   tags = {
@@ -22,52 +34,18 @@ resource "aws_subnet" "public-b" {
 }
 
 
-resource "aws_subnet" "public-c" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.7.0/24"
-
-  tags = {
-    Name = "public-c-tf"
-  }
-}
-
-resource "aws_subnet" "private-a" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.3.0/24"
-
-  tags = {
-    Name = "private-a-tf"
-  }
-}
-
-resource "aws_subnet" "private-b" {
-  vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.4.0/24"
-
-  tags = {
-    Name = "private-b-tf"
-  }
-}
-
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16"
-  
-  tags = {
-    Name = "terraform"
-  }
-}
-
-
+####GTW
 resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.default.id
+  vpc_id = aws_vpc.vpc_terraform_tp.id
 
   tags = {
     Name = "igw-tf"
   }
 }
 
+####ROUTE TABLE
 resource "aws_route_table" "r" {
-  vpc_id = aws_vpc.default.id
+  vpc_id = aws_vpc.vpc_terraform_tp.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -83,83 +61,79 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.r.id
 }
 
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.public-b.id
-  route_table_id = aws_route_table.r.id
-}
-
-resource "tls_private_key" "example" {
+####PRIVATE KEY
+resource "tls_private_key" "pk" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "ec2-key-tf"
-  public_key = tls_private_key.example.public_key_openssh
+  public_key = tls_private_key.pk.public_key_openssh
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+####ALB
+resource "aws_lb" "alb_terraform" {
+  name               = "alb_terraform"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.allow_http.id}"]
+  subnets            = ["${aws_subnet.subnet_1_terraform.id}"]
+}
+
+resource "aws_lb_listener" "alb_listner_terraform" {
+  load_balancer_arn = aws_lb.alb_terraform.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  vpc_terraform_tp_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb_target_group_tf.arn
+  }
+}
+
+
+####TARGET GROUP
+resource "aws_lb_target_group" "lb_target_group_tf" {
+  name     = "lb_target_group_tf"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.vpc_terraform_tp.id
+}
+resource "aws_lb_target_group_attachment" "target_group_attachment_tf" {
+  target_group_arn = aws_lb_target_group.lb_target_group_tf.arn
+  target_id        = aws_instance.lb_target_group_tf.id
+  port             = 80
+}
+
+####AUTOSCALING
+resource "aws_autoscaling_group" "autoscaling_tf" {
+  name                      = "as-terraform-tp"
+  max_size                  = 3
+  min_size                  = 1
+  health_check_grace_period = 20
+  health_check_type         = "ELB"
+  desired_capacity          = 1
+  force_delete              = true
+  placement_group           = "${aws_placement_group.test.id}"
+  launch_configuration      = "${aws_launch_configuration.foobar.name}"
+  vpc_zone_identifier       = ["${aws_subnet.public-a.id}", "${aws_subnet.public-b.id}"]
+
+  initial_lifecycle_hook {
+    name                 = "autoscaling"
+    vpc_terraform_tp_result       = "CONTINUE"
+    heartbeat_timeout    = 2000
+    lifecycle_transition = "autoscaling:EC2_INSTANCE_LAUNCHING"
+    notification_target_arn = "arn:aws:sqs:us-east-1:444455556666:queue1*"
+    role_arn                = "arn:aws:iam::123456789012:role/S3Access"
+  }
+} 
+
+####LAUNCH CONFIGURATION
+resource "aws_launch_configuration" "launch_configuration_terraform" {
+  image_id = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public-b.id
-  key_name      = aws_key_pair.deployer.id
-  associate_public_ip_address = true
-  user_data     = file("${path.module}/postinstall.sh")
-  vpc_security_group_ids =  ["${aws_security_group.allow_http.id}"]
-
-  tags = {
-    Name = "HelloWorld"
-  }
+  security_groups = aws_security_group.allow_http.id
+  user_data = file("${path.module}/post_install.sh")
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical 
-}
-
-resource "aws_security_group" "allow_http" {
-  name        = "allow_tls"
-  description = "Allow http inbound traffic"
-  vpc_id      = "${aws_vpc.default.id}"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  egress {
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_http-tf"
-  }
-}
-
-output "private-key" {
-  value = tls_private_key.example.private_key_pem
-}
-
-output "ami-value" {
-  value = data.aws_ami.ubuntu.image_id
-}
-
-output "public-ip" {
-  value = aws_instance.web.public_ip
-}
